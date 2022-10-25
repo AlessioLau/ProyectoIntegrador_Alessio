d1c4e687ac12363f0965a31b80660037a37702a6
AlessioLau <lautaroalessio@gmail.com>
2022-10-25 09:13:13 -0300

    Agregado de bootstrap al proeycto
    Html y css de: Acerca de mi, Banner, Header, Logo AP, Social
    E inicio de desarrollo de las partes educacion y experiencia

--- ProyectoIntegrador_Alessio/src/app/app.module.ts
+++ ProyectoIntegrador_Alessio/src/app/app.module.ts
@@ -8,6 +8,9 @@ import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { XwingComponent } from './components/xwing/xwing.component';
 import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,10 @@ import { BannerComponent } from './components/banner/banner.component';
     LogoAPComponent,
     XwingComponent,
     SocialComponent,
-    BannerComponent
+    BannerComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule,
